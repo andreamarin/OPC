7bd4ed3cd633b07766eb5bf7df543ab1ca26a231
sdist <sdist@itam.mx>
2019-11-05 09:01:10 -0600

    ejercicios BM1

--- ejercicios/ensamblador/OpArrArg.asm
+++ ejercicios/ensamblador/OpArrArg.asm
@@ -16,7 +16,7 @@ mnul=0h
 
 .DATA
 ; PROC main
-arrayi SDWORD 40h, 20h, 30h, 50h, 10h
+arrayi SDWORD 40h, 20h, 30h, 60h, 10h
 nuni DWORD LENGTHOF arrayi
 suma SDWORD ?
 arrayr SDWORD 5 DUP(?)
@@ -26,14 +26,24 @@ txtr BYTE mcr,mlf,"DumpMem de arrayr.",mnul
 adios BYTE mcr,mlf,"ADIOS.",mnul
 
 ; PROC sycArrdw, variables locales
-arrd1 DWORD ?
+arr1d SDWORD 5 DUP(?)
+unidades DWORD ?
+arr2d SDWORD 5 DUP (?)
+dirRet SDWORD ?
 
 .CODE
 main PROC
 
     ; call sycArrdw(arrayi, nuni, arrayr)
-
+    push offset arrayi
+    push nuni
+    push OFFSET arrayr 
     call sycArrdw
+
+    pop suma
+    mov eax, suma
+    call WriteInt
+    call CrLf
 
 
     ; Despliega un dump de memoria de arrayi.
@@ -68,17 +78,40 @@ sycArrdw PROC
 ; Recibe
 ;     Stack:
 ; Regresa
-;     Stack;	
+;     Stack; Suma de los elementos de arr2d	
 ; Varibles automaticas y locales
 
 ;   Argumentos o parametros pasados
-
-    mov ebx, 2
-    .WHILE ebx < 1
+    pop dirRet
+    pop arr2d       ; OFFSET arrayr
+    pop unidades    ; OFFSET nuni
+    pop arr1d       ; OFFSET arrayi
     
+    mov eax, 0      ; guardo suma
+    mov ebx, 0      ; contador
+    mov esi, arr1d  ; direccionamiento indirecto
+    mov edi, arr2d  ; 
+
+    
+    .WHILE ebx < unidades
+        mov ecx, [esi]
+        sub ecx, 30h
+        
+        mov [edi], ecx
+        add eax, ecx
+        
+        add esi, TYPE arr1d
+        add edi, TYPE arr2d
+        
+        inc ebx
     .ENDW
+
+
+    push eax
+    push dirRet
+    
+    RET             ; return
 	
-    RET
 sycArrdw ENDP
 
 END main
---
+++ ejercicios/ensamblador/ejercicioBM1.asm
@@ -0,0 +1,158 @@
+TITLE *MASM Template	(ejerBj)*
+
+; Descripcion:
+; 
+; 
+
+INCLUDE \masm32\Irvine\Irvine32.inc
+INCLUDELIB \masm32\Irvine\Irvine32.lib
+INCLUDELIB \masm32\Irvine\User32.lib
+INCLUDELIB \masm32\Irvine\Kernel32.lib
+
+.DATA
+
+N DWORD ?
+M DWORD ?
+textoLec BYTE "Ingrese un número de temperaturas a leer entre 1 y 10",0
+textoError1 BYTE "Error por N < 1 o N > 10",0
+
+textoLectura1 BYTE "Teclee la ",0
+textoLectura2 BYTE " temperatura: ",0
+
+min SDWORD 300000
+pos DWORD 0
+
+textoMin BYTE "Minimo de las temperaturas: ",0
+
+textoFin1 BYTE "Temperatura ",0
+textoFin2 BYTE ": ",0
+
+textoNumInt BYTE "Numero de intercambios: ",0
+adios BYTE "ADIOS", 0
+
+temperaturas SDWORD 10 DUP(?)
+
+;aux SDWORD 800
+
+.CODE
+; Procedimiento principal
+main PROC
+
+    MOV ESI, OFFSET temperaturas               ; Ubicamos en ESI el lugar de memoria del arreglo de temperaturas
+    
+    MOV EDX, OFFSET textoLec                   ; Leemos el número de temperaturas que se van a ingresar
+    CALL WriteString
+    CALL CrLf
+    CALL ReadInt
+    
+    MOV N, EAX                                 ; Almacenamos el total de temperaturas a leer
+    .IF N < 1 || N > 10                        ; Si el número de temperaturas es menor a 1 o mayor
+        MOV EDX, OFFSET textoError1            ; a 10 se emite un error
+        CALL WriteString
+        CALL CrLf
+    .ELSE
+        MOV EBX, 1                             ; Iniciamos un contador en 1
+        .WHILE EBX <= N                        ; Leemos las N temperaturas
+            MOV EDX, OFFSET textoLectura1
+            CALL WriteString
+            MOV EAX, EBX
+            CALL WriteInt
+            MOV EDX, OFFSET textoLectura2
+            CALL WriteString
+            CALL ReadInt
+            CALL CrLf
+
+            MOV [ESI], EAX                     ; Almacenamos la temperatura en el arreglo de temperaturas
+            ADD ESI, TYPE temperaturas         ; Incrementamos la dirección de memoria almacenada en ESI por 4
+
+            .IF EAX < min                      ; Si la temperatura ingresada es menor a la temperatura mínima
+                MOV min, EAX                   ; se almacena como la nueva temperatura mínima
+                MOV pos, EBX
+            .ENDIF
+            INC EBX                            ; Se incrementa el contador
+        .ENDW
+        
+        MOV EDX, OFFSET textoMin
+        CALL WriteString
+        MOV EAX, min
+        CALL WriteInt
+        CALL CrLf
+
+        MOV ECX, N
+        DEC ECX
+        MOV M, ECX                             ; M = N-1
+
+        MOV ECX, 0                             ; Contador de intercambios
+        MOV EBX, 0                             ; Contador
+        MOV ESI, OFFSET temperaturas
+        
+        .WHILE EBX < M
+
+            MOV EDI, ESI
+            ADD EDI, TYPE temperaturas         ; apuntador 2
+
+            MOV EDX, EBX                       ; contador 2
+            INC EDX
+            
+            .WHILE EDX < N
+                MOV EAX, SDWORD PTR [ESI] 
+                            
+                .IF EAX > SDWORD PTR [EDI]                 
+                    XCHG SDWORD PTR [EDI], EAX            ; intercambiamos
+                    
+                    MOV SDWORD PTR [ESI], EAX
+                    INC ECX
+                .ENDIF
+
+                INC EDX                        ; incrementa contador
+                ADD EDI, TYPE temperaturas
+            .ENDW
+
+            INC EBX
+            ADD ESI, TYPE temperaturas
+
+        .ENDW 
+
+        CALL CrLf
+        
+        MOV EAX, ECX
+        MOV EDX, OFFSET textoNumInt
+        CALL WriteString
+        CALL WriteInt
+        CALL CrLf            
+
+        MOV EBX, 1
+        MOV ESI, OFFSET temperaturas
+        .WHILE EBX <= N
+            MOV EDX, OFFSET textoFin1
+            CALL WriteString
+            MOV EAX, EBX
+            CALL WriteInt
+            MOV EDX, OFFSET textoFin2
+            CALL WriteString
+            
+            MOV EAX, SDWORD PTR [ESI]
+            CALL WriteInt
+            
+            ADD ESI, TYPE temperaturas         ; Decrementamos la dirección de memoria contenida en ESI
+            INC EBX                            ; Decrementamos el contador
+    
+            CALL CrLf
+    
+        .ENDW
+    
+    .ENDIF
+
+    ;MOV EAX, [temperaturas + 8]
+    ;CALL WriteInt
+
+    CALL CrLf
+    MOV EDX, OFFSET adios
+    CALL WriteString
+
+    exit
+main ENDP
+; Termina el procedimiento principal
+
+; Termina el area de Ensamble
+END main
